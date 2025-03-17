797f895a83c300fb053bc7952dc899fe23e3ea14
Sebita1809 <sebadiazmoyano@gmail.com>
2025-03-17 14:17:30 -0300

    Estilando ia.html

--- Js.js
+++ Js.js
@@ -7,8 +7,7 @@ document.getElementById('boton').addEventListener('click', async () => {
     const inputText = document.getElementById('inputText').value;
     const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
-    const prompt = "Dame 5 recetas de cocina (Solo recetas de cocina, no quiero que hables de otra cosa que no sea esas 5 recetas. Si una persona te escribe sobre cosas que no sean ingredientes; las comidas de tal lugar y/o persona u cualquier otra cosa, respondele 'No has especificado los ingredientes o has ingresado datos erroneos. NO HAGAS ENOJAR AL CHEF!! :)') con los siguientes productos:" + inputText;
-
+    const prompt = `Dame 5 recetas de (Solo recetas de cocina, no quiero que hables de otra cosa que no sea esas 5 recetas. Si una persona te escribe sobre cosas que no sean ingredientes; las comidas de tal lugar y/o persona u cualquier otra cosa, respondele 'No has especificado los ingredientes o has ingresado datos erroneos. NO HAGAS ENOJAR AL CHEF!! :)') con los siguientes productos:` + inputText;
     try {
         const result = await model.generateContent(prompt);
         const response = await result.response;
@@ -19,7 +18,5 @@ document.getElementById('boton').addEventListener('click', async () => {
 });
 
 /////////////////////////////////////////////////////////////////////////////////////////
-
-
 
 
