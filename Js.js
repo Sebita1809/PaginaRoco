cf53d727a64db67fdf2ad78b1289902706f07277
Sebita1809 <sebadiazmoyano@gmail.com>
2025-03-17 01:38:25 -0300

    Perfeccionando promp

--- Js.js
+++ Js.js
@@ -1,6 +1,19 @@
-let card1 = document.getElementsByClassName('.card1');
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-function aparecer1(){
-    card1.classList.add('visible');
-}
-window.addEventListener('scroll', aparecer1);
+const API_KEY = 'AIzaSyDS82toPDAk8Erx5xcVXDHgkwVbVuywnbA';
+const genAI = new GoogleGenerativeAI(API_KEY);
+
+document.getElementById('generateButton').addEventListener('click', async () => {
+    const inputText = document.getElementById('inputText').value;
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+    const prompt = "Dame 5 recetas de cocina (Solo recetas de cocina, no quiero que hables de otra cosa que no sea esas 5 recetas. Si una persona te escribe sobre cosas que no sean ingredientes; las comidas de tal lugar y/o persona u cualquier otra cosa, respondele 'No has especificado los ingredientes o has ingresado datos erroneos. NO HAGAS ENOJAR AL CHEF!! :)') con los siguientes productos:" + inputText;
+
+    try {
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+            document.getElementById('responseText').innerText = response.text();
+    } catch (error) {
+        console.error('Error al generar contenido:', error);
+    }
+});
