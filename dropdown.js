4a3a9d44d4dc25b9e674cbe243c85ae809328349
Sebita1809 <sebadiazmoyano@gmail.com>
2025-03-21 00:52:03 -0300

    ..

--- dropdown.js
+++ dropdown.js
@@ -1,20 +1,14 @@
 function toggleDropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
     const flecha = document.getElementById("flechaDropdown");
-    if (flecha.src.includes('imagenes/flechaAbajo.png')) {
-        flecha.src = 'imagenes/flechaArriba.png';
-    } else {
-        flecha.src = 'imagenes/flechaAbajo.png';
-    }
 }
 // Cierra el dropdown si el usuario hace clic fuera de él
 window.onclick = function(event) {
-    if (!event.target.matches('.dropDownPerfil button img')) {
+    if (!event.target.matches('.home button img')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         for (var i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
-                document.getElementById("flechaDropdown").src = 'imagenes/flechaAbajo.png';
                 openDropdown.classList.remove('show');
             }
         }
